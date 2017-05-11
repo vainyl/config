dcb1f9fac1ae71efa9a137eab32bf5ec466e661d
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-05-11 12:50:15 +0300

    Fixed copy issues

--- src/Chain/ConfigSourceChain.php
+++ src/Chain/ConfigSourceChain.php
@@ -94,7 +94,7 @@ class ConfigSourceChain extends AbstractSource implements ConfigSourceInterface
         /**
          * @var ConfigSourceInterface $source
          */
-        foreach (clone $this->sources as $source) {
+        foreach (($copy = $this->sources->copy()) as $source) {
             if (null === ($configData = $source->getData($descriptor))) {
                 continue;
             }
