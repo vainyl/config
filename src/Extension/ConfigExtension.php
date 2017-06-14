1816bf40ec7b6fe64db2be5a65adaa661fd04955
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-14 13:52:54 +0300

    Fixed config descriptor

--- src/Extension/ConfigExtension.php
+++ src/Extension/ConfigExtension.php
@@ -30,25 +30,6 @@ class ConfigExtension extends AbstractExtension
      */
     public function load(array $configs, ContainerBuilder $container): AbstractExtension
     {
-        foreach ($configs as $config) {
-            if (false === array_key_exists('config', $config)) {
-                continue;
-            }
-
-            $config = $config['config'];
-            if (false === array_key_exists('factory', $config)) {
-                throw new MissingRequiredFieldException($container, 'config', $config, 'factory');
-            }
-
-            $factoryConfig = $config['factory'];
-            if (false === array_key_exists('factory', $config)) {
-                throw new MissingRequiredFieldException($container, 'config', $factoryConfig, 'class');
-            }
-            $arguments = $factoryConfig['arguments'] ?? [];
-            $container
-                ->setDefinition('config.factory', new Definition($config['class'], $arguments));
-        }
-
         $container
             ->addCompilerPass(new ConfigCompilerPass())
             ->addCompilerPass(new ConfigSourceCompilerPass());
